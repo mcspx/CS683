30aeb0cf1789c8d1180568d3ef70888b0a8465c8
Fixed Broadband <fixed@Fixeds-MacBook-Pro.local>
2018-12-22 10:01:44 +0700

    Update Document

--- Assignment/01-OpenMP/sum.cpp
+++ Assignment/01-OpenMP/sum.cpp
@@ -8,11 +8,11 @@ int main(void){
         int ID = omp_get_thread_num();
         //int x[1000];
         
-        int i;
-        //initialization
+        int i; //initialization
         for(i=ID*1;i<(ID+1)*1;i++){
             sum[ID]+=i;
         }
+
         printf("L0-->core:%d, sum:%d \n",ID,sum[ID]);
 #pragma omp barrier
         if(!(ID%2)){
