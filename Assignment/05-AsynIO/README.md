0e106f971bf2e25ba5a3c715823dac10b07d95a2
Fixed Broadband <fixed@Fixeds-MacBook-Pro.local>
2018-12-25 02:18:22 +0700

    Update AsyncIO Optimize

--- Assignment/05-AsynIO/README.md
+++ Assignment/05-AsynIO/README.md
@@ -1,2 +1,4 @@
 AsynIO
 	- Web crawler using AsynIO
+	- Try to optimize for visit & procress 100 website in 30 seconds
+	- Best time : 40.791 seconds. (Internet speed slow)
---
+++ Assignment/05-AsynIO/app.py
@@ -0,0 +1,71 @@
+import asyncio
+import aiohttp
+import time
+
+from bs4 import BeautifulSoup
+
+#----------------------------------------
+
+async def visit(client, queue, sites):
+    try:
+        urls = queue.pop(0)
+
+        async with client.get(urls, timeout=30) as response:
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
+
+                for href in soup.findAll('a'):
+                    link = str(href.get('href'))
+
+                    if link.startswith('http'):
+                        if link not in sites:
+                            queue.append(link)
+
+                            #print('New - ', link)
+                        #else:
+                            #print('Dup - ', link)
+
+                sites.append(urls)
+
+                return urls
+            else:
+                print('Request error ...')
+
+                return ''
+    except:
+        print('Exception error ...')
+
+        return ''
+
+# ----------------------------------------
+
+async def main(queue, sites):
+    limit = 100
+    count = 0
+
+    while len(queue) > 0 and count < limit:
+        count = count + 1
+
+        async with aiohttp.ClientSession() as client:
+            urls = await visit(client, queue, sites)
+
+            if urls:
+                print('Visit OK - %04d : %s' % (count, urls))
+            else:
+                print('Visit error !')
+
+# ----------------------------------------
+
+if __name__ == '__main__':
+    elapsed_start = time.time()
+
+    queue = ['https://en.wikipedia.org/wiki/Main_Page']
+    sites = []
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(queue, sites))
+
+    elapsed_finish = time.time()
+
+    print('Elapsed :', (elapsed_finish - elapsed_start), 'seconds.')
